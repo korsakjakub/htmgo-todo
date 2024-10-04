d8f789ca3e89d51edf5514108a42d2827f964908
Jakub Korsak <jk@MacBook-Air-Jakub.local>
2024-10-04 21:04:57 +0200

    htmgo-init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module todo
+
+go 1.23.0
+
+require github.com/maddalax/htmgo/framework v0.0.0-20241004162115-805e5729e1ce
+
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"embed"
+	"github.com/maddalax/htmgo/framework/h"
+	"github.com/maddalax/htmgo/framework/service"
+	"io/fs"
+	"net/http"
+	"todo/__htmgo"
+)
+
+//go:embed assets/dist/*
+var StaticAssets embed.FS
+
+func main() {
+	locator := service.NewLocator()
+
+	h.Start(h.AppOpts{
+		ServiceLocator: locator,
+		LiveReload:     true,
+		Register: func(app *h.App) {
+			sub, err := fs.Sub(StaticAssets, "assets/dist")
+
+			if err != nil {
+				panic(err)
+			}
+
+			http.FileServerFS(sub)
+
+			app.Router.Handle("/public/*", http.StripPrefix("/public", http.FileServerFS(sub)))
+			__htmgo.Register(app.Router)
+		},
+	})
+}
